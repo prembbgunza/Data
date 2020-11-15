0f9008d7121c721b6660188419f0fbcda3b9512f
prembbgunza <69067798+prembbgunza@users.noreply.github.com>
2020-11-15 22:16:24 +0700

    Update PHP เรียก bootstrap modal มาแสดงผลัพพธ์ (ยังไม่ได้ผล).php

--- Code/PHP เรียก bootstrap modal มาแสดงผลัพพธ์ (ยังไม่ได้ผล).php
+++ Code/PHP เรียก bootstrap modal มาแสดงผลัพพธ์ (ยังไม่ได้ผล).php
@@ -7,7 +7,7 @@
 	<title>ฟอร์ม</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
 	integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-	      crossorigin="anonymous">
+	      	crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
 	integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
